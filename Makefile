e7990d66d9937fc83cfe6499c2d13150ea847b86
Andrzej <vim>
2018-07-23 15:45:29 +0200

    First commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+CXX = clang++
+STD = -std=c++11
+CXXFLAGS = -Wall -Wpedantic -Wextra -Wc++11-extensions
+SRCS = $(wildcard *.cpp)
+OBJS = $(SRCS:.cpp=.o)
+TARGET = a.out
+
+.PHONY: all
+all:$(TARGET)
+
+%.o: %.cpp  
+	$(CXX) $(CXXFLAGS) $(STD) -c -o $@ $<
+
+$(TARGET): $(OBJS)
+	$(CXX)  $^ -o $@ 
+
+.PHONY: clean
+clean:
+	rm $(TARGET) $(OBJS)
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "Student.hpp"
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<Student> v;
+  v.push_back(Student(1, "Kowalska1", "Anna1"));
+  v.push_back(Student(2, "Kow2", "Anna2"));
+  v.push_back(Student(3, "Kowalska3", "Anna3"));
+  v.push_back(Student(4, "Kowalska4", "Anna4"));
+
+  // save to file
+  std::ofstream out_file("./data.txt");
+  for (auto z : v) {
+    out_file << z.getIndex() << std::endl << z.getName() << std::endl << z.getSurname() << std::endl;
+  }
+  out_file.close();
+  // read from file
+  std::vector<Student> v2;
+  std::ifstream input_file("./data.txt");
+  if (input_file.is_open()) {
+    int a;
+    std::string b, c;
+    while (input_file >> a >> b >> c) {
+      v2.push_back(Student(a, b, c));
+    }
+    input_file.close();
+  } else {
+    std::cout << "Unable to open file";
+  }
+  // print data read from file and added to vector
+  for (auto z : v2) {
+    std::cout << z.getIndex() << " " << z.getSurname() << " " << z.getName() << std::endl;
+  }
+  return 0;
+}
